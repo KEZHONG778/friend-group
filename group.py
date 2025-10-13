c9ab41860931960e2baa678d451f325eeff41543
Ke Zhong <ucapkz0@ucl.ac.uk>
2025-10-13 16:30:20 +0100

    add max age in different conditions

--- group.py
+++ group.py
@@ -69,3 +69,45 @@ if __name__ == "__main__":
         for relation, people in info["relations"].items():
             print(f"  - {relation}: {', '.join(people)}")
         print()
+
+def max_age():
+   
+    max_age = max(info['age'] for info in my_group.values())        
+    print(f"max age of the group is {max_age}")
+
+max_age()    
+
+def avg_relations():
+    total_relation = sum(len(info['relations']) for info in my_group.values())
+    avg_realtions = total_relation/len(my_group)
+    print(f"the average relationship  number is{avg_realtions}")
+
+avg_relations()    
+
+def max_age_with_relationship():
+    age_with_relationship = []
+
+    for info in my_group.values():
+        if info ['relations'] :
+            age_with_relationship.append(info ['age'])
+
+    max_age_value_with_relationship = max(age_with_relationship)   
+    print(f"the max age with relationship is {max_age_value_with_relationship} ")     
+
+max_age_with_relationship()    
+
+def max_age_with_friends():
+    ages_with_friends = []
+    
+    for info in my_group.values():
+        if 'friend' in info['relations']:  
+            ages_with_friends.append(info['age'])
+    
+    if ages_with_friends: 
+        max_age_value = max(ages_with_friends)
+    else:
+        max_age_value = 0
+    
+    print(f"max age with friends: {max_age_value}")
+    
+max_age_with_friends()
