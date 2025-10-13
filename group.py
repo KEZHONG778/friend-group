16b97686f1ae00e2a2e49e1ac9510b3a36770ca8
Ke Zhong <ucapkz0@ucl.ac.uk>
2025-10-13 10:09:39 +0100

    create relationship

--- group.py
+++ group.py
@@ -2,4 +2,44 @@
 
 # Your code to go here...
 
-my_group =
+my_group ={
+    "Jill": {
+        "age": 26,
+        "job": "biologist",
+        "relations": {
+            "friend": ["Zalika"],
+            "partner": ["John"]
+        }
+    },
+    "Zalika": {
+        "age": 28, 
+        "job": "artist",
+        "relations": {
+            "friend": ["Jill"]
+        }
+    },
+    "John": {
+        "age": 27,
+        "job": "writer", 
+        "relations": {
+            "partner": ["Jill"],
+            "cousin": ["Nash"]
+        }
+    },
+    "Nash": {
+        "age": 34,
+        "job": "chef",
+        "relations": {
+            "cousin": ["John"],
+            "landlord": ["Zalika"]  
+        }
+    }
+}
+
+
+if __name__ == "__main__":
+    for name, info in my_group.items():
+        print(f"{name} ({info['age']}age, {info['job']}) relationship：")
+        for relation, people in info["relations"].items():
+            print(f"  - {relation}: {', '.join(people)}")
+        print()
